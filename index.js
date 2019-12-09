f2923dd34464cab3425e089d84b7848a5c8ca964
Nick Meyvis <nick.meyvis@gmail.com>
2019-12-08 22:56:42 -0500

    update server file paths

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const isProd = process.argv[2] === "prod";
 const fs = require("fs");
 const httpProxy = require("http-proxy");
 
-const Rooms = require("./server/Rooms.js");
+const Rooms = require("./server/src/Rooms.js");
 const rooms = new Rooms.Rooms();
 
-const ForceHttpsServer = require("./server/servers/ForceHttpsServer.js");
-const AppServer = require("./server/servers/AppServer.js");
-const StaticFilesServer = require("./server/servers/StaticFilesServer.js");
-const ApiServer = require("./server/servers/ApiServer.js");
-const SocketServer = require("./server/servers/SocketServer.js");
-const Tracker = require("./server/servers/TrackerServer.js");
+const ForceHttpsServer = require("./server/src/servers/ForceHttpsServer.js");
+const AppServer = require("./server/src/servers/AppServer.js");
+const StaticFilesServer = require("./server/src/servers/StaticFilesServer.js");
+const RoomApiServer = require("./server/src/servers/RoomApiServer.js");
+const SocketServer = require("./server/src/servers/SocketServer.js");
+const Tracker = require("./server/src/servers/TrackerServer.js");
 
-const WebServerFactory = require("./server/WebServerFactory.js");
+const WebServerFactory = require("./server/src/WebServerFactory.js");
 
 let serverFactory = new WebServerFactory({
     key: fs.readFileSync("./certs/privkey.pem", "utf8"),
@@ -36,7 +36,7 @@ new AppServer(serverFactory, "../../client/dist", rooms, socketServer.io).listen
 
 socketServer.server.listen(4000);
 
-new ApiServer(serverFactory, rooms).listen(5000);
+new RoomApiServer(serverFactory, rooms).listen(5000);
 
 new Tracker().listen(8001);
 
